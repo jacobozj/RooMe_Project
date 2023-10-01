a4f78324140ba026af7a6eec6fb9aa5d1ab705d9
Jacobo Zuluaga <jacobozuluaga80@gmail.com>
2023-10-01 16:55:46 -0500

    cambios barra search

--- RooMe/urls.py
+++ RooMe/urls.py
@@ -18,7 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('pages.urls')),
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
-    path('accounts/', include('accounts.urls'))
+    path('accounts/', include('accounts.urls')),
 ]
