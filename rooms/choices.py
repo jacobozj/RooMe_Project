a7e7ab83255773ae91ef24b5ad131083f13d51c0
Jacobo Zuluaga <jacobozuluaga80@gmail.com>
2023-10-04 19:42:15 -0500

    cambios en la pagina de habitaciones, html y css

---
+++ rooms/choices.py
@@ -0,0 +1,62 @@
+bedroom_choices = {
+    '1':1,
+    '2':2,
+    '3':3,
+    '4':4,
+    '5':5,
+    '6':6,
+    '7':7,
+    '8':8,
+    '9':9,
+    '10':10
+}
+
+
+price_choices = {
+    '1000000': '$1,000,000',
+    '2000000': '$2,000,000',
+    '3000000': '$3,000,000',
+    '4000000': '$4,000,000',
+    '5000000': '$5,000,000',
+    '6000000': '$6,000,000',
+    '7000000': '$7,000,000',
+    '8000000': '$8,000,000',
+    '9000000': '$9,000,000',
+    '10000000': '$10,000,000+',
+}
+
+
+state_choices = {
+    'AMA': 'Amazonas',
+    'ANT': 'Antioquia',
+    'ARA': 'Arauca',
+    'ATL': 'Atlántico',
+    'BOL': 'Bolívar',
+    'BOY': 'Boyacá',
+    'CAL': 'Caldas',
+    'CAQ': 'Caquetá',
+    'CAS': 'Casanare',
+    'CAU': 'Cauca',
+    'CES': 'Cesar',
+    'CHO': 'Chocó',
+    'COR': 'Córdoba',
+    'CUN': 'Cundinamarca',
+    'GUA': 'Guainía',
+    'GUV': 'Guaviare',
+    'HUI': 'Huila',
+    'GUV': 'La Guajira',
+    'MAG': 'Magdalena',
+    'MET': 'Meta',
+    'NAR': 'Nariño',
+    'NDS': 'Norte de Santander',
+    'PUT': 'Putumayo',
+    'QUI': 'Quindío',
+    'RIS': 'Risaralda',
+    'SAP': 'San Andrés y Providencia',
+    'SAN': 'Santander',
+    'SUC': 'Sucre',
+    'TOL': 'Tolima',
+    'VAL': 'Valle del Cauca',
+    'VAU': 'Vaupés',
+    'VID': 'Vichada'
+}
--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Rooms
 
 # Create your views here.
 def index(request):
     rooms=Rooms.objects.all()
     return render(request, 'pages/index.html', {'rooms': rooms})
+
+def room(request, id):
+    rooms = get_object_or_404(Rooms, pk=id)
+
+    context = {
+        'rooms': rooms
+    }
+
+    return render(request, 'rooms/rooms.html', context)
