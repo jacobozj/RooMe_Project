3e776118212ad8dfaa6f91799bea3b0746b4adcd
unknown <simobotero7445@gmail.com>
2023-10-19 08:52:35 -0500

    cambios en models de rooms, y front de about y rooms

--- rooms/models.py
+++ rooms/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 class Rooms(models.Model):
     title=models.CharField(max_length=50,default="")
-    description=models.CharField(max_length=100)
+    description=models.CharField(max_length=200)
     price=models.IntegerField()
     location=models.CharField(max_length=20)
     reserved=models.BooleanField(default=False)
@@ -14,7 +15,9 @@ class Rooms(models.Model):
     city=models.CharField(max_length=20,default="")
     state=models.CharField(max_length=20,default="")
     image=models.ImageField(upload_to='rooms/images/', default="")
-    image2=models.ImageField(upload_to='rooms/images/', default="")
-    image3=models.ImageField(upload_to='rooms/images/', default="")
-    image4=models.ImageField(upload_to='rooms/images/', default="")
-    image5=models.ImageField(upload_to='rooms/images/', default="")
+    image2=models.ImageField(upload_to='rooms/images/', default="", blank=True)
+    image3=models.ImageField(upload_to='rooms/images/', default="", blank=True)
+    image4=models.ImageField(upload_to='rooms/images/', default="", blank=True)
+    image5=models.ImageField(upload_to='rooms/images/', default="", blank=True)
+    fecha=models.DateField(default=datetime.now, blank=True)
+    renter=models.CharField(max_length=50, default="")
